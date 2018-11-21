a983c4181b582f99f89a6a1ddf36592e5077e29e
Bryan M. Karpowicz <bkarpowi@discover16.prv.cube>
2018-11-21 15:09:17 -0500

    fix iasiiasiCrisAirs_inBufrSubset_allStandard.py module load order

--- iasiCrisAirs_inBufrSubset_allStandard.py
+++ iasiCrisAirs_inBufrSubset_allStandard.py
@@ -1,10 +1,8 @@
-t!/usr/bin/env python2.7
+#!/usr/bin/env python2.7
 # sorry, 2.7 for now, you can run 2to3 on rttov, but I need to get my scripts together to automate that a bit.
 rttovPath = "/discover/nobackup/bkarpowi/rt/rttov12_gcc7.2/"
 import matplotlib
 matplotlib.use('Agg')
-import pyrttov
-import example_data as ex
 import numpy as np
 import os
 import sys
@@ -20,6 +18,8 @@ import h5py
 pyRttovPath = os.path.join(rttovPath,'wrapper')
 if not pyRttovPath in sys.path:
     sys.path.append(pyRttovPath)
+import pyrttov
+import example_data as ex
 
 def calculateWeightingFunctions(chan_list, rttovInstance, myProfiles):
     
